41c15926641c22b1ef91bd584b55efb37dac3b3a
Tobias Schramm <tobleminer@gmail.com>
2020-04-17 23:15:01 +0200

    Add GitHub CI

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+if [[ "$EUID" -eq 0 ]]; then
+	pacman --noconfirm -Syu
+	pacman --noconfirm -S base-devel arm-none-eabi-gcc cpio bc git rsync perl python sudo mtd-utils unzip wget
+	useradd build
+	chown -R build:build .
+	sudo -u build ./build.sh
+else
+	makepkg
+	sha256sum *.tar.xz > SHA256SUMS
+	cp SHA256SUMS release_info.txt
+fi
